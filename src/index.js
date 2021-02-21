c75eb741e350f4253500fcedc9daa0666b601a17
MarinaKryuchko88 <68333655+MarinaKryuchko88@users.noreply.github.com>
2021-02-21 20:22:16 +0300

    work on bugs

--- src/index.js
+++ src/index.js
@@ -2,37 +2,21 @@ import { inputEl, countriesListEl } from './js/refs';
 import { countriesMarkup, oneCountryMarkup } from './js/articles-markup';
 import debounce from 'lodash.debounce';
 import error from './js/notifications';
-// import fetchCountries from './js/fetch-articles';
+// import fetchCountries from './js/fetch-countries';
 import './js/notifications';
 
 inputEl.addEventListener(
   'input',
   debounce(event => {
     event.preventDefault();
-    // console.log(event.target.value); // одно и
-    // console.log(inputEl.value);  // то же
-
-    // fetchCountries(countryName).then(data => {
-    //   console.log(data);
-    //   if (data.length === 1) {
-    //     oneCountryMarkup(data);
-    //   }
-    //   if (data.length >= 2 && data.length <= 10) {
-    //     countriesMarkup(data); // потому что data у нас - это уже массив объектов.
-    //   }
-    //   if (data.length > 10) {
-    //     error({
-    //       text: 'Too many matches found. Please enter a more specific query!',
-    //     });
-    //   }
-    // inputEl.value = '';
-
-    // console.log(data.length);
-    // countriesListEl.innerHTML = '';
-    // });
+    // console.log(event.target.value); // одно и то же // console.log(inputEl.value);
 
     inputEl.textContent = inputEl.value;
     const countryName = inputEl.textContent;
+
+    if (countryName === ' ') {
+      return;
+    }
 
     const url = `https://restcountries.eu/rest/v2/name/${countryName}`;
     fetch(url)
@@ -40,6 +24,7 @@ inputEl.addEventListener(
       .then(data => {
         if (data.length === 1) {
           oneCountryMarkup(data);
+          inputEl.value = '';
         }
         if (data.length >= 2 && data.length <= 10) {
           countriesMarkup(data); // потому что data у нас - это уже массив объектов.
@@ -49,10 +34,28 @@ inputEl.addEventListener(
             text: 'Too many matches found. Please enter a more specific query!',
           });
         }
-        inputEl.value = '';
       })
       .catch(error => console.log(error));
 
     countriesListEl.innerHTML = '';
-  }, 500),
+  }, 600),
 );
+
+// fetchCountries(countryName).then(data => {
+//   console.log(data);
+//   if (data.length === 1) {
+//     oneCountryMarkup(data);
+//   }
+//   if (data.length >= 2 && data.length <= 10) {
+//     countriesMarkup(data); // потому что data у нас - это уже массив объектов.
+//   }
+//   if (data.length > 10) {
+//     error({
+//       text: 'Too many matches found. Please enter a more specific query!',
+//     });
+//   }
+// inputEl.value = '';
+
+// console.log(data.length);
+// countriesListEl.innerHTML = '';
+// });
